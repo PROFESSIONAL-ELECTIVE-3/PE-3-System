2b3a115ecbcca33f2922b3fd671b5df393fe335e
Rei0133 <reychee05@gmail.com>
2026-08-19 20:17:11 +0800

    ..

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,7 +12,6 @@ const Home = () => {
         <ul className="navbar-links">
           <li><a href="#home">Home</a></li>
           <li><a href="#features">Features</a></li>
-          <li><a href="#dashboard">Dashboard</a></li>
           <li><a href="#about">About</a></li>
         </ul>
         <div className="navbar-actions">
